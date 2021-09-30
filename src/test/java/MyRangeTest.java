7a0e0866a3a608de628b0a312c00c13e8e8d80a6
Ppich00 <49331622+Ppich00@users.noreply.github.com>
2021-09-30 12:02:51 +0700

    case 02 :: falseStartWithInclude

--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -1,19 +1,27 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyRangeTest {
 
     @Test
     @DisplayName("input ตัวแรก เป็น [ input = [1,5] result = true")
-    public void case01(){
+    public void case01() {
         String input = "[1,5]";
         MyRange myRange = new MyRange(input);
         boolean result = myRange.starWithInclude();
         assertTrue(result);
     }
 
+    @Test
+    @DisplayName("input ตัวแรก เป็น [ input = (1,5] result = false")
+    public void case02() {
+        String input = "(1,5]";
+        MyRange myRange = new MyRange(input);
+        boolean result = myRange.starWithInclude();
+        assertFalse(result);
+    }
 
 }

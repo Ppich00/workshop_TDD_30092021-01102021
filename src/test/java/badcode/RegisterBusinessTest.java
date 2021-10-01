43c19b5ea7fb288d9447ce0ebc47e171343a2053
Ppich00 <49331622+Ppich00@users.noreply.github.com>
2021-10-01 10:15:57 +0700

    case08 :: success : 01

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -1,5 +1,6 @@
 package badcode;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,5 +13,92 @@ class RegisterBusinessTest {
         RegisterBusiness registerBusiness = new RegisterBusiness();
         Exception exception = assertThrows(ArgumentNullException.class, () -> registerBusiness.register(null, new Speaker()));
         assertEquals("First name is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Exception : Last name is required.")
+    void case02() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        Exception exception = assertThrows(ArgumentNullException.class, () -> registerBusiness.register(null, speaker));
+        assertEquals("Last name is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Exception : Email is required.")
+    void case03() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        Exception exception = assertThrows(ArgumentNullException.class, () -> registerBusiness.register(null, speaker));
+        assertEquals("Email is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Exception : DomainEmailInvalidException")
+    void case04() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        speaker.setEmail("Email");
+        Exception exception = assertThrows(DomainEmailInvalidException.class, () -> registerBusiness.register(null, speaker));
+    }
+
+    @Test
+    @DisplayName("Exception : Speaker doesn't meet our standard rules.")
+    void case05() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        speaker.setEmail("Email@xxx.com");
+        Exception exception = assertThrows(SpeakerDoesntMeetRequirementsException.class, () -> registerBusiness.register(null, speaker));
+        assertEquals("Speaker doesn't meet our standard rules.", exception.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("Exception : Can't save a speaker.")
+    void case06() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        speaker.setEmail("Email@gmail.com");
+        Exception exception = assertThrows(SaveSpeakerException.class, () -> registerBusiness.register(null, speaker));
+        assertEquals("Can't save a speaker.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Exception : Can't save a speaker.")
+    void case07() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        speaker.setEmail("Email@gmail.com");
+        Exception exception = assertThrows(SaveSpeakerException.class, () -> registerBusiness.register(null, speaker));
+        assertEquals("Can't save a speaker.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("success : 01")
+    void case08() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FirstName");
+        speaker.setLastName("LastName");
+        speaker.setEmail("Email@gmail.com");
+
+        int id = registerBusiness.register(new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1000;
+            }
+        }, speaker);
+        assertEquals(1000,id);
     }
 }
